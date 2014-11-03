a8f9c0c81c25ed1710184dcb58fde5fe895dbd00
nimalikularathne88@gmail.com <nimalikularathne88@gmail.com@a17dee98-9a91-0718-6e07-63f35f8f7078>
2014-11-03 11:26:48 +0000

    childcare dummy data

---
+++ trunk/childcare/database/data/data_dummy/probation_unit.sql
@@ -0,0 +1,5 @@
+USE child_db;
+
+TRUNCATE TABLE probation_unit;
+INSERT INTO `probation_unit` VALUES ('PB01', 'Probation Unit, Kandy', 'NONE', 7, '0812345678','NONE', 'Kandy', 'Nothing' , 1, 'U001', 'U001', '2014/11/01', '2014/11/01' );
+
