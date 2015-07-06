5493cfa5e1ada0f73439acbe7f2634878b393059
chamilka.jayarathna <chamilka.jayarathna@a17dee98-9a91-0718-6e07-63f35f8f7078>
2015-07-06 06:11:58 +0000

    police station list highlight selected values in database

--- childcare/src/pdn/sci/cs/action/ProbationUnitAction.java
+++ childcare/src/pdn/sci/cs/action/ProbationUnitAction.java
@@ -20,251 +20,272 @@ import pdn.sci.cs.service.SystemUserService;
 @Scope(value = "prototype")
 public class ProbationUnitAction extends BaseAction {
 
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  @Autowired
-  private ProbationUnitService probationUnitService;
-  @Autowired
-  private PoliceStationService policeStationService;
-  @Autowired
-  private SystemUserService systemUserService;
-  @Autowired
-  private DistrictService districtService;
+	@Autowired
+	private ProbationUnitService probationUnitService;
+	@Autowired
+	private PoliceStationService policeStationService;
+	@Autowired
+	private SystemUserService systemUserService;
+	@Autowired
+	private DistrictService districtService;
 
-  private static Logger logger = Logger.getLogger(ProbationUnitAction.class);
+	private static Logger logger = Logger.getLogger(ProbationUnitAction.class);
 
-  private List<ProbationUnit> list;
-  private List<PoliceStation> policeStationList;
-  private ProbationUnit probationUnit;
-  private List<SystemUser> probationOfficers;
-  private String referenceId;
-  private List<String> selectedPoliceStations;
-  private String districtId;
-  private List<District> districtList;
+	private List<ProbationUnit> list;
+	private List<PoliceStation> policeStationList;
+	private ProbationUnit probationUnit;
+	private List<SystemUser> probationOfficers;
+	private String referenceId;
+	private List<String> selectedPoliceStations;
+	private String districtId;
+	private List<District> districtList;
+	private String districtIdSelected;
 
-  private static final String provincial_officer = "Provincial Officer";
+	private static final String provincial_officer = "Provincial Officer";
 
-  public String list() {
+	public String list() {
 
-    if ((getSessionUser().getUserRole().equals(SystemUser.USER_ROLE.ADMIN.name()) || getSessionUser()
-        .getUserRole().equals(SystemUser.USER_ROLE.MINISTRY.name()))) {
-      list = probationUnitService.findAll();
-      return SUCCESS;
-    } else {
-      String referenceId = getSessionUser().getReferenceId();
-      if (getSessionUser().getUserRole().equals(SystemUser.USER_ROLE.OFFICER.name())) {
-        if (getSessionUser().getPost().equals(provincial_officer)) {
-          list = probationUnitService.findByProvince(referenceId);
-          return SUCCESS;
-        } else {
-          ProbationUnit punit = probationUnitService.findById(referenceId);
-          list = new ArrayList<ProbationUnit>();
-          list.add(punit);
-          return SUCCESS;
-        }
-      } else {
-        return INPUT;
-      }
-    }
-  }
+		if ((getSessionUser().getUserRole().equals(
+				SystemUser.USER_ROLE.ADMIN.name()) || getSessionUser()
+				.getUserRole().equals(SystemUser.USER_ROLE.MINISTRY.name()))) {
+			list = probationUnitService.findAll();
+			return SUCCESS;
+		} else {
+			String referenceId = getSessionUser().getReferenceId();
+			if (getSessionUser().getUserRole().equals(
+					SystemUser.USER_ROLE.OFFICER.name())) {
+				if (getSessionUser().getPost().equals(provincial_officer)) {
+					list = probationUnitService.findByProvince(referenceId);
+					return SUCCESS;
+				} else {
+					ProbationUnit punit = probationUnitService
+							.findById(referenceId);
+					list = new ArrayList<ProbationUnit>();
+					list.add(punit);
+					return SUCCESS;
+				}
+			} else {
+				return INPUT;
+			}
+		}
+	}
 
-  public String searchForm() {
-    return SUCCESS;
-  }
+	public String searchForm() {
+		return SUCCESS;
+	}
 
-  public String frame() {
-    return SUCCESS;
-  }
+	public String frame() {
+		return SUCCESS;
+	}
 
-  public String search() {
-    list = probationUnitService.search(probationUnit);
-    return SUCCESS;
-  }
+	public String search() {
+		list = probationUnitService.search(probationUnit);
+		return SUCCESS;
+	}
 
-  public String add() {
-    addMode();
-    return SUCCESS;
-  }
+	public String add() {
+		addMode();
+		return SUCCESS;
+	}
 
-  public String edit() {
-    editMode();
-    return view();
-  }
+	public String edit() {
+		editMode();
+		return view();
+	}
 
-  public String save() {
+	public String save() {
 
-    if (probationUnit != null) {
-      validateProbationUnit();
-      if (hasErrors()) {
-        return INPUT;
-      } else {
-        if (operationMode == OPERATION_MODE.ADD && probationUnit.getId().isEmpty()) {
-          setAddSettings(probationUnit);
-          addPoliceStations();
-          fieldsGenerators();
-          probationUnit = probationUnitService.save(probationUnit);
-        } else if (operationMode == OPERATION_MODE.EDIT && !probationUnit.getId().isEmpty()) {
-          setUpdateSettings(probationUnit);
-          addPoliceStations();
-          fieldsGenerators();
-          probationUnitService.update(probationUnit);
-        } else {
-          addActionError("Error");
-          return INPUT;
-        }
+		if (probationUnit != null) {
+			validateProbationUnit();
+			if (hasErrors()) {
+				return INPUT;
+			} else {
+				if (operationMode == OPERATION_MODE.ADD
+						&& probationUnit.getId().isEmpty()) {
+					setAddSettings(probationUnit);
+					addPoliceStations();
+					fieldsGenerators();
+					probationUnit = probationUnitService.save(probationUnit);
+				} else if (operationMode == OPERATION_MODE.EDIT
+						&& !probationUnit.getId().isEmpty()) {
+					setUpdateSettings(probationUnit);
+					addPoliceStations();
+					fieldsGenerators();
+					probationUnitService.update(probationUnit);
+				} else {
+					addActionError("Error");
+					return INPUT;
+				}
 
-      }
-    } else {
-      addActionError("Invalid Access");
-      return INPUT;
-    }
+			}
+		} else {
+			addActionError("Invalid Access");
+			return INPUT;
+		}
 
-    this.id = probationUnit.getId();
-    return view();
-  }
-  
-  private void fieldsGenerators(){
-    probationUnit.setDistrict(districtService.findById(probationUnit.getDistrict().getId()));
-    
-  }
+		this.id = probationUnit.getId();
+		return view();
+	}
 
-  private void addPoliceStations() {
-    if (this.probationUnit != null && this.selectedPoliceStations != null) {
-      if (probationUnit.getPoliceStations() == null) {
-        probationUnit.setPoliceStations(new HashSet<PoliceStation>());
-      }
-      for (String s : selectedPoliceStations) {
-        probationUnit.getPoliceStations().add(policeStationService.findById(s));
-      }
-    }
-  }
+	private void fieldsGenerators() {
+		/*probationUnit.setDistrict(districtService.findById(probationUnit
+				.getDistrict().getId()));*/
+		probationUnit.setDistrict(districtService.findById(this.districtIdSelected));
+	}
 
-  public String view() {
+	private void addPoliceStations() {
+		if (this.probationUnit != null && this.selectedPoliceStations != null) {
+			if (probationUnit.getPoliceStations() == null) {
+				probationUnit.setPoliceStations(new HashSet<PoliceStation>());
+			}
+			for (String s : selectedPoliceStations) {
+				probationUnit.getPoliceStations().add(
+						policeStationService.findById(s));
+			}
+		}
+	}
 
-    if (id == null || id.isEmpty()) {
-      addActionError("Invalid Access");
-      return INPUT;
-    } else {
+	public String view() {
 
-      probationUnit = probationUnitService.findById(id);
-      if (probationUnit == null) {
-        addActionError("Item that your are searching could not be found");
-        return INPUT;
-      }
+		if (id == null || id.isEmpty()) {
+			addActionError("Invalid Access");
+			return INPUT;
+		} else {
 
-      referenceId = id;
-      probationOfficerList();
+			probationUnit = probationUnitService.findById(id);
+			if (probationUnit == null) {
+				addActionError("Item that your are searching could not be found");
+				return INPUT;
+			}
 
-      if (operationMode == OPERATION_MODE.EDIT) {
-        selectedPoliceStations = new ArrayList<String>();
-        
-        for (PoliceStation ps : probationUnit.getPoliceStations()) {
-          selectedPoliceStations.add(ps.getId());
-        }
-        districtId = probationUnit.getDistrict().getId();
-      }
-    }
-    return SUCCESS;
-  }
+			referenceId = id;
+			probationOfficerList();
 
-  public String delete() {
-    if (this.id.isEmpty()) {
-      addActionError("Could not delete the entry, id is missing");
-      return INPUT;
-    } else {
-      probationUnitService.delete(id);
-      return list();
-    }
+			if (operationMode == OPERATION_MODE.EDIT) {
+				selectedPoliceStations = new ArrayList<String>();
 
-  }
+				for (PoliceStation ps : probationUnit.getPoliceStations()) {
+					selectedPoliceStations.add(ps.getId());
+				}
+				districtId = probationUnit.getDistrict().getId();
+			}
+		}
+		return SUCCESS;
+	}
 
-  public String probationOfficerList() {
-    if (referenceId != null) {
-      probationOfficers = systemUserService.search(SystemUser.USER_ROLE.OFFICER, referenceId);
-    } else {
-      probationOfficers = new ArrayList<SystemUser>();
-    }
+	public String delete() {
+		if (this.id.isEmpty()) {
+			addActionError("Could not delete the entry, id is missing");
+			return INPUT;
+		} else {
+			probationUnitService.delete(id);
+			return list();
+		}
 
-    return SUCCESS;
-  }
+	}
 
-  private void validateProbationUnit() {
-    if (this.probationUnit.getName().isEmpty()) {
-      addFieldError("probationUnit.name", "Name cannot be empty");
-    }
+	public String probationOfficerList() {
+		if (referenceId != null) {
+			probationOfficers = systemUserService.search(
+					SystemUser.USER_ROLE.OFFICER, referenceId);
+		} else {
+			probationOfficers = new ArrayList<SystemUser>();
+		}
 
-    if (this.probationUnit.getDistrict() == null
-        || this.probationUnit.getDistrict().getId().isEmpty()) {
-      addFieldError("probationUnit.district.id", "District cannot be empty");
-    }
-  }
+		return SUCCESS;
+	}
 
-  public List<ProbationUnit> getList() {
-    return list;
-  }
+	private void validateProbationUnit() {
+		if (this.probationUnit.getName().isEmpty()) {
+			addFieldError("probationUnit.name", "Name cannot be empty");
+		}
 
-  public void setList(List<ProbationUnit> list) {
-    this.list = list;
-  }
+		/*
+		 * if (this.probationUnit.getDistrict() == null ||
+		 * this.probationUnit.getDistrict().getId().isEmpty()) {
+		 * addFieldError("probationUnit.district.id",
+		 * "District cannot be empty"); }
+		 */
+		if (this.districtIdSelected==null || this.districtIdSelected.isEmpty()) {
+			addFieldError("probationUnit.district.id",
+					"District cannot be empty");
+		}
+	}
 
-  public ProbationUnit getProbationUnit() {
-    return probationUnit;
-  }
+	public List<ProbationUnit> getList() {
+		return list;
+	}
 
-  public void setProbationUnit(ProbationUnit probationUnit) {
-    this.probationUnit = probationUnit;
-  }
+	public void setList(List<ProbationUnit> list) {
+		this.list = list;
+	}
 
-  public List<PoliceStation> getPoliceStationList() {
-    policeStationList = policeStationService.findAll();
-    return policeStationList;
-  }
+	public ProbationUnit getProbationUnit() {
+		return probationUnit;
+	}
 
-  public void setPoliceStationList(List<PoliceStation> policeStationList) {
-    this.policeStationList = policeStationList;
-  }
+	public void setProbationUnit(ProbationUnit probationUnit) {
+		this.probationUnit = probationUnit;
+	}
 
-  public String getReferenceId() {
-    return referenceId;
-  }
+	public List<PoliceStation> getPoliceStationList() {
+		policeStationList = policeStationService.findAll();
+		return policeStationList;
+	}
 
-  public void setReferenceId(String referenceId) {
-    this.referenceId = referenceId;
-  }
+	public void setPoliceStationList(List<PoliceStation> policeStationList) {
+		this.policeStationList = policeStationList;
+	}
 
-  public List<SystemUser> getProbationOfficers() {
-    return probationOfficers;
-  }
+	public String getReferenceId() {
+		return referenceId;
+	}
 
-  public void setProbationOfficers(List<SystemUser> probationOfficers) {
-    this.probationOfficers = probationOfficers;
-  }
+	public void setReferenceId(String referenceId) {
+		this.referenceId = referenceId;
+	}
 
-  public List<String> getSelectedPoliceStations() {
-    return selectedPoliceStations;
-  }
+	public List<SystemUser> getProbationOfficers() {
+		return probationOfficers;
+	}
 
-  public void setSelectedPoliceStations(List<String> selectedPoliceStations) {
-    this.selectedPoliceStations = selectedPoliceStations;
-  }
+	public void setProbationOfficers(List<SystemUser> probationOfficers) {
+		this.probationOfficers = probationOfficers;
+	}
 
-  public List<District> getDistrictList() {
-    districtList = districtService.findAll();
-    return districtList;
-  }
+	public List<String> getSelectedPoliceStations() {
+		return selectedPoliceStations;
+	}
 
-  public void setDistrictList(List<District> districtList) {
-    this.districtList = districtList;
-  }
+	public void setSelectedPoliceStations(List<String> selectedPoliceStations) {
+		this.selectedPoliceStations = selectedPoliceStations;
+	}
 
-public String getDistrictId() {
-	return districtId;
-}
+	public List<District> getDistrictList() {
+		districtList = districtService.findAll();
+		return districtList;
+	}
 
-public void setDistrictId(String districtId) {
-	this.districtId = districtId;
-}
+	public void setDistrictList(List<District> districtList) {
+		this.districtList = districtList;
+	}
 
+	public String getDistrictId() {
+		return districtId;
+	}
 
+	public void setDistrictId(String districtId) {
+		this.districtId = districtId;
+	}
+
+	public String getDistrictIdSelected() {
+		return districtIdSelected;
+	}
+
+	public void setDistrictIdSelected(String districtIdSelected) {
+		this.districtIdSelected = districtIdSelected;
+	}
 
 }
